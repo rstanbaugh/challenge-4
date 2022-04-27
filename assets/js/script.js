1edac0863095264f408ec65c558d27e8d4b5d7c6
rstanbaugh <rich_bootcamp@icloud.com>
2022-04-26 22:06:21 -0400

    working html and css mockup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 // Assignment Code
-var startBtn = document.querySelector("#startQuiz");
+var startBtn = document.querySelector("#start-quiz");
 
 function startQuiz () {
   window.alert("quiz started");
 }
 
+var readFile = function(){
+  // Check for the various File API support.
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+    alert("success");
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
+};
+
+var readFile = function(){
+  File.FileReader
+};
+
+var startQuiz = function(){
+
+};
 
 // Add event listener to generate button
 startBtn.addEventListener("click", startQuiz);
