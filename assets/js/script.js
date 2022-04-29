b5d989d04264ba6fc7348296adf99220fc18c94a
rstanbaugh <rich_bootcamp@icloud.com>
2022-04-29 03:25:13 -0400

    stuck on second question not loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,21 @@ var currentQuestion = 0;
 var bodyEl = document.querySelector("#page"); 
 var startBtn = document.querySelector("#start-quiz");
 
-// create main container
-var mainEl = document.createElement("div");
-mainEl.className = "main";
-
 // create header element
 var headerEl = document.createElement("div");
 headerEl.className = "header";
 headerEl.innerHTML = "<h4>View Highscores</h4>" +
   "<h4>Time: 0</h4>" +
   "</header>";
+// load header
+bodyEl.appendChild(headerEl);
+
+// create main container
+var mainEl = document.createElement("div");
+  mainEl.className = "main";
+// load main
+bodyEl.appendChild(mainEl)
+
 
 // create intro element in a div
 var introEl = document.createElement("div");
@@ -41,6 +46,15 @@ var correctAnswerEl = document.createElement("h2")
 var wrongAnswerEl = document.createElement("h2")
   wrongAnswerEl.className = "results";
   wrongAnswerEl.innerHTML = "<h2>Wrong Answer!!</h2>";
+
+// present intro page
+var loadIntro = function(){
+    // clear main element of the intro html
+    mainEl.innerHTML ="";
+    
+    // build intro page
+    mainEl.appendChild(introEl);
+};
 
 
 // load quiz questions
@@ -73,64 +87,72 @@ var fetchQuizData = function(){
   }
   ];
   return myQuiz;
-}
+};
 
 // function to build html for current question
 var getQuestion = function(currentQuestion){
-  var answerString="";
-  while (currentQuestion < 1){
-    quizEl.innerHTML = "<h1>" + myQuiz[currentQuestion].question + "</h1>" + answerString;
+var answerString="";
+  quizEl.innerHTML = "<h1>" + myQuiz[currentQuestion].question + "</h1>" + answerString;
 
-    // create ul of answers
-    var answerListEl = document.createElement("ul");
-      answerListEl.id = "answers";
-  
-   // add the answers as li
-    for (var j=0; j <  myQuiz[currentQuestion].answers.length; j++) {
-      var answerEl = document.createElement("li");
-        answerEl.setAttribute("data-answer-num",j+1);
-        answerEl.textContent = (j+1)+". "+myQuiz[currentQuestion].answers[j];
-        answerListEl.appendChild(answerEl);
-    };
-    quizEl.appendChild(answerListEl);
-    currentQuestion++;
-  };
+// create ul of answers
+var answerListEl = document.createElement("ul");
+  answerListEl.id = "answers";
+
+// add the answers as li
+for (var j=0; j <  myQuiz[currentQuestion].answers.length; j++) {
+  var answerEl = document.createElement("li");
+    answerEl.setAttribute("data-answer-num",j+1);
+    answerEl.textContent = (j+1)+". "+myQuiz[currentQuestion].answers[j];
+    answerListEl.appendChild(answerEl);
+};
+quizEl.appendChild(answerListEl);
+currentQuestion++;
 };
 
-var Quiz = function() {
-  // window.alert("quiz started");
-
+var quiz = function() {
   // clear main element of the intro html
   mainEl.innerHTML ="";
-
-  // present the question
+  // push question to the DOM
   mainEl.appendChild(quizEl);
-}
+};
 
 var handleAnswer = function(event){
+  alert("answer handler")
   answerSelected = event.target.getAttribute("data-answer-num");
   if (answerSelected === myQuiz[currentQuestion].correctAnswer){
     quizEl.appendChild(correctAnswerEl);
-    alert("CORRECT");
   } else {
     quizEl.appendChild(wrongAnswerEl);
-    alert (answerSelected+"<>"+myQuiz[currentQuestion].correctAnswer);
   }
+
+  // move to next question
+  currentQuestion++;
+  if (currentQuestion <= parseInt(myQuiz.length,10)){
+    getQuestion(currentQuestion);
+    quiz();
+  } else {
+    endgame();
+  }
+
 }
 
 
 // code executing here
+// -------------------
+
+// fetch the data for quiz questions and answers
 var myQuiz = fetchQuizData();
 // preload the first question
 getQuestion(currentQuestion);
-bodyEl.appendChild(headerEl);
-bodyEl.appendChild(mainEl)
-mainEl.appendChild(introEl);
+
+// load the intro
+loadIntro();
+
 
 
 
 
 
 // Add event listener to generate button
-startBtnEl.addEventListener("click", Quiz);
+startBtnEl.addEventListener("click", quiz);
 quizEl.querySelector("#answers").addEventListener("click", handleAnswer)
