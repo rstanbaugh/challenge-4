05671f06365cf4baa1caa1bd10790ca414f2b4e5
rstanbaugh <rich_bootcamp@icloud.com>
2022-04-29 17:51:32 -0400

    added timer, reconstituted index.html & all JS constructors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,38 @@
 // variables
 var currentQuestion = 0;
+var timeLeft = 30;  // initial time left in game
 var bodyEl = document.querySelector("#page"); 
 var startBtn = document.querySelector("#start-quiz");
-
-// create header element
-var headerEl = document.createElement("div");
-headerEl.className = "header";
-headerEl.innerHTML = "<h4>View Highscores</h4>" +
-  "<h4>Time: 0</h4>" +
-  "</header>";
-// load header
-bodyEl.appendChild(headerEl);
-
-// create main container
-var mainEl = document.createElement("div");
-  mainEl.className = "main";
-// load main
-bodyEl.appendChild(mainEl)
-
-// Main container has been constructed
-//  Body
-//    -> header   
-//    -> main
+var introEl = document.querySelector("#intro");
+var quizEl = document.querySelector("#quiz");
+var startBtnEl = document.querySelector("#start-quiz");
+var timerEl = document.querySelector("#timer");
+var finalScoreEl = document.querySelector("#final-score");
 
 
-// create intro element in a div 
-// to hold all html for main section of intro page)
-var introEl = document.createElement("div");
-  introEl.className = "intro";
-  introEl.innerHTML = " <h1>Coding Quiz Challenge</h1>" +
-  "<p>Try to answer the following code-related questions within the time limit. " +
-  "Keep in mind that incorrect answers will penaliez your score/time by ten seconds.</p>";
 
-// create start button element and append to introEl
-var startBtnEl = document.createElement("button");
-  startBtnEl.className = "btn";
-  startBtnEl.id = "start-quiz";
-  startBtnEl.textContent = "Start Quiz";
-  introEl.appendChild(startBtnEl);
-//  end intro
-
-
-// create quiz element to hold all html for main section of quiz page
-var quizEl = document.createElement("div");
-  quizEl.className = "quiz";
+// load quiz questions
+var myQuiz = [{
+  question: "Commonly used data types DO NOT include:",
+  answers: ["strings", "booleans", "alerts", "numbers"],
+  correctAnswer: "3"},
+{
+  question: "The condition in an if / else statement is enclosed within _____.",
+  answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+  correctAnswer: "3"},
+{
+  question: "Arrays in JavaScript can be used to store ____.",
+  answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+  correctAnswer: "4"},
+{
+  question: "String values must be enclosed within ____ when being assigned to variables.",
+  answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+  correctAnswer: "3"},
+{
+  question: "A very useful tool during development and debugging of printing content to the debugger is: ",
+  answers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+  correctAnswer: "4"}
+];
 
 // create correct answer element - shown when question answered correctly
 var correctAnswerEl = document.createElement("h2")
@@ -57,47 +46,29 @@ var wrongAnswerEl = document.createElement("h2")
 //  end initial quiz element section
 
 
+var gameTimer = function(){
+  var timer = setInterval(function(){
+    if(timeLeft <= 0){
+      clearInterval(timer);
+      timerEl.innerHTML = "0";
+      endGame(timeLeft);
+    } else if (timeLeft === "done"){
+      clearInterval(timer);
+      timerEl.innerHTML = "0";
+    } else {
+      timerEl.innerHTML = timeLeft + "s";
+    }
+    timeLeft -= 1;
+  }, 1000);
+}
+
+
+
 // load intro page
 var loadIntro = function(){
-    // clear main element of the intro html
-    mainEl.innerHTML ="";
-    
-    // build intro page
-    mainEl.appendChild(introEl);
+introEl.style.display = "";
 };
 
-
-// load quiz questions
-var fetchQuizData = function(){
-  var myQuiz = [
-  {
-    question: "Commonly used data types DO NOT include:",
-    answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: "3"
-  },
-  {
-    question: "The condition in an if / else statement is enclosed within _____.",
-    answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    correctAnswer: "3"
-  },
-  {
-    question: "Arrays in JavaScript can be used to store ____.",
-    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswer: "4"
-  },
-  {
-    question: "String values must be enclosed within ____ when being assigned to variables.",
-    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
-    correctAnswer: "3"
-  },
-  {
-    question: "A very useful tool during development and debugging of printing content to the debugger is: ",
-    answers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    correctAnswer: "4"
-  }
-  ];
-  return myQuiz;
-};
 
 // function to build html for next question
 var nextQuestion = function(currentQuestion){
@@ -113,60 +84,58 @@ var nextQuestion = function(currentQuestion){
     var answerEl = document.createElement("li");
       answerEl.setAttribute("data-answer-num",j+1);
       answerEl.textContent = (j+1)+". "+myQuiz[currentQuestion].answers[j];
+      answerEl.addEventListener("click", handleAnswer);
       answerListEl.appendChild(answerEl);
   };
   quizEl.appendChild(answerListEl);
 };
 
+
 var startQuiz = function() {
-  mainEl.innerHTML ="";         // clear main element of the intro html
-  mainEl.appendChild(quizEl);   // push question to the DOM
+  introEl.style.display = "none";
+  nextQuestion(currentQuestion);
+  gameTimer();
 };
+
+// called when out of time or all questions answered
+var endGame = function(score){
+  timeLeft="done";
+  quizEl.style.display = "none";
+
+  var pageContent = document.createElement("div");
+  pageContent.innerHTML ="<h1>All Done!</h1><br>"+
+    "Your final score is:".concat(score);
+  
+  finalScoreEl.appendChild(pageContent);
+  
+  finalScoreEl.style.display = ""
+};
+
 
 // event handler for answer click
 var handleAnswer = function(event){
-  // alert("answer handler")
   answerSelected = event.target.getAttribute("data-answer-num");
   if (answerSelected === myQuiz[currentQuestion].correctAnswer){
     quizEl.appendChild(correctAnswerEl);
+    timeLeft += 10;
   } else {
     quizEl.appendChild(wrongAnswerEl);
+    timeLeft -= 10;
   }
 
   // move to next question
   currentQuestion++;
-  if (currentQuestion <= parseInt(myQuiz.length,10)){
+  if (currentQuestion < myQuiz.length){
     nextQuestion(currentQuestion);
-    // startQuiz();
   } else {
-    endgame();
+    endGame(timeLeft);
   }
 
 }
 
-// called when game ends
-// - out of time
-// - out of questions
-var endGame = function(){
-  alert("end game!");
-};
-
-
-// to be develeped later - game timer
-var gameLeft = function(){
-  return true;
-}
-
-
-// quiz execution begins here
-var myQuiz = fetchQuizData();   // fetch the data for quiz questions and answers
-nextQuestion(currentQuestion);  // preload the first question
-loadIntro();                    // load the intro page
 
 
 
 
 // Event listeners
 startBtnEl.addEventListener("click", startQuiz);   // start button on intro page
-// gameTime.addEventListener()
-quizEl.querySelector("#answers").addEventListener("click", handleAnswer)  // quizEl has to be set before calling
